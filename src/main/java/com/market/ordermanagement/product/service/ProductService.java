6f4b6d98332c995622a4671067f77fd0fecdd178
PratikshaShanbhag <42670233+PratikshaShanbhag@users.noreply.github.com>
2021-09-13 11:40:48 +0530

    Add files via upload

---
+++ src/main/java/com/market/ordermanagement/product/service/ProductService.java
@@ -0,0 +1,121 @@
+package com.market.ordermanagement.product.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.market.ordermanagement.product.dto.ProductDTO;
+import com.market.ordermanagement.product.entity.Product;
+import com.market.ordermanagement.product.exception.InfyMarketException;
+import com.market.ordermanagement.product.repository.ProductRepository;
+import com.market.ordermanagement.product.repository.SubscribedProdRepository;
+
+@Service
+public class ProductService {
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	ProductRepository productrepo;
+
+	@Autowired
+	SubscribedProdRepository subscribedprodrepo;
+
+	// Get products by name
+	public List<ProductDTO> getProductByName(@PathVariable String productname) throws InfyMarketException {
+
+		logger.info("Product request with name {}", productname);
+
+		Iterable<Product> product = productrepo.findByproductname(productname);
+		List<ProductDTO> productDTO = new ArrayList<ProductDTO>();
+
+		product.forEach(Pro -> {
+			productDTO.add(ProductDTO.valueOf(Pro));
+		});
+		if (productDTO.isEmpty())
+			throw new InfyMarketException("Service.PRODUCTS_NOT_FOUND");
+		return productDTO;
+	}
+
+	//Get products by category
+	public List<ProductDTO> getProductBycategory(@PathVariable String category) throws InfyMarketException {
+		logger.info("Product request of category {}", category);
+		Iterable<Product> product = productrepo.findBycategory(category);
+		List<ProductDTO> productDTO = new ArrayList<ProductDTO>();
+
+		product.forEach(Pro -> {
+			productDTO.add(ProductDTO.valueOf(Pro));
+		});
+		if (productDTO.isEmpty())
+			throw new InfyMarketException("Service.PRODUCTS_NOT_FOUND");
+		return productDTO;
+	}
+
+	//Get products by seller id
+	public List<ProductDTO> getProductBySellerId(@PathVariable String sellerid) throws InfyMarketException {
+		logger.info("Product request of seller {}", sellerid);
+		Iterable<Product> product = productrepo.findBysellerid(sellerid);
+		List<ProductDTO> productDTO = new ArrayList<ProductDTO>();
+
+		product.forEach(Pro -> {
+			productDTO.add(ProductDTO.valueOf(Pro));
+		});
+		if (productDTO.isEmpty())
+			throw new InfyMarketException("Service.PRODUCTS_NOT_FOUND");
+		return productDTO;
+	}
+
+	//Get all product details
+	public List<ProductDTO> getAllProduct() throws InfyMarketException {
+
+		Iterable<Product> products = productrepo.findAll();
+		List<ProductDTO> productDTOs = new ArrayList<>();
+
+		products.forEach(product -> {
+			ProductDTO productDTO = ProductDTO.valueOf(product);
+			productDTOs.add(productDTO);
+		});
+		if (productDTOs.isEmpty())
+			throw new InfyMarketException("Service.PRODUCTS_NOT_FOUND");
+		logger.info("Product Details : {}", productDTOs);
+		return productDTOs;
+	}
+
+	//Update stock
+	public Product updateProductStock(Product product, String prodid) throws InfyMarketException{
+        Product existingProduct = productrepo.findById(prodid).orElse(null);
+        if(existingProduct != null) {
+            existingProduct.setStock(product.getStock());
+            return productrepo.save(existingProduct); 
+        }else {
+        	throw new InfyMarketException("Service.PRODUCTS_NOT_FOUND");
+        }
+    
+    }
+	
+	//Get product by id
+	public ProductDTO getByProdid(String prodid) throws InfyMarketException{
+		logger.info("Profile request for customer {}", prodid);
+		Product pro = productrepo.findByProdid(prodid);
+		if(pro != null) {
+			ProductDTO proDTO = ProductDTO.valueOf(pro);
+			logger.info("Profile for customer : {}", proDTO);
+			return proDTO;
+		} else{
+			throw new InfyMarketException("Service.PRODUCTS_NOT_FOUND");
+		}
+	}
+	
+	//Add product
+	public void saveProduct(ProductDTO productDTO) throws InfyMarketException {
+		logger.info("Adding product with data {}", productDTO);
+		Product product = productDTO.createProduct();
+		productrepo.save(product);
+	}
+
+
+}
---
+++ src/main/java/com/market/ordermanagement/product/service/SubscribedProdService.java
@@ -0,0 +1,56 @@
+package com.market.ordermanagement.product.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.market.ordermanagement.product.dto.SubscribedProdDTO;
+import com.market.ordermanagement.product.entity.SubscribedProd;
+import com.market.ordermanagement.product.exception.InfyMarketException;
+import com.market.ordermanagement.product.repository.SubscribedProdRepository;
+
+@Service
+public class SubscribedProdService {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	SubscribedProdRepository subscribedprodrepo;
+	
+	//Get subscribed product of buyer
+	public List<SubscribedProdDTO> getByBuyerid(@PathVariable String buyerid) throws InfyMarketException{
+
+		logger.info("Products for the buyer with id {}", buyerid);
+
+		Iterable<SubscribedProd> sub = subscribedprodrepo.findByBuyerid(buyerid);
+		List<SubscribedProdDTO> subscribedprodDTO = new ArrayList<SubscribedProdDTO>();
+
+		sub.forEach(subs -> {
+			subscribedprodDTO.add(SubscribedProdDTO.valueOf(subs));
+		});
+		if(subscribedprodDTO.isEmpty())
+			throw new InfyMarketException("Service.EMPTY_BUYER_PRODUCTS");
+		return subscribedprodDTO;
+	}
+	
+	//Get all subscribed products
+	public List<SubscribedProdDTO> getAllSubProduct() throws InfyMarketException{
+
+		Iterable<SubscribedProd> subs = subscribedprodrepo.findAll();
+		List<SubscribedProdDTO> subscribedprodDTOs = new ArrayList<>();
+
+		subs.forEach(subpro -> {
+			SubscribedProdDTO subscribedprodDTO = SubscribedProdDTO.valueOf(subpro);
+			subscribedprodDTOs.add(subscribedprodDTO);
+		});
+		if(subscribedprodDTOs.isEmpty())
+			throw new InfyMarketException("Service.NO_SUBSCRIBED_PRODUCTS");
+		logger.info("Subscribed Product Details : {}", subscribedprodDTOs);
+		return subscribedprodDTOs;
+	}
+	
+}
